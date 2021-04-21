d866e4e36400874a770905cb2300c69066e8de3d
Paul Lucaci <paul.lucaci@icloud.com>
2021-04-21 19:35:59 +0100

    Restricted to Linear Sarsa, only

--- frozen_lake.py
+++ frozen_lake.py
@@ -1,39 +1,14 @@
-from rl.environments.tabular import FrozenLake
-from rl.environments.tabular import LinearWrapper
+from sys import path
 
-from rl.algorithms.model_based import policy_iteration
-from rl.algorithms.model_based import value_iteration
+path.append("frozen_lake.py")
+path.append("tabular.py")
 
-from rl.algorithms.model_free import sarsa
-from rl.algorithms.model_free import q_learning
-
-from rl.algorithms.model_free import linear_sarsa
-from rl.algorithms.model_free import linear_q_learning
-
-from rl.algorithms.model_building import ps_policy_iteration
-
-
-def play(env):
-    actions = ['w', 'a', 's', 'd']
-    
-    state = env.reset()
-    env.render()
-    
-    done = False
-    while not done:
-        c = input('\nMove: ')
-        if c not in actions:
-            raise Exception('Invalid action')
-            
-        state, r, done = env.step(actions.index(c))
-        
-        env.render()
-        print('Reward: {0}.'.format(r))
-
+from tabular import FrozenLake, LinearWrapper
+from model_free import linear_sarsa
 
 def main():
     seed = 0
-    
+
     # Big lake
     # lake = [['&', '.', '.', '.', '.', '.', '.', '.'],
     #         ['.', '.', '.', '.', '.', '.', '.', '.'],
@@ -43,7 +18,7 @@ def main():
     #         ['.', '#', '#', '.', '.', '.', '#', '.'],
     #         ['.', '#', '.', '.', '#', '.', '#', '.'],
     #         ['.', '.', '.', '#', '.', '.', '.', '$']]
-    
+
     # Small lake
     lake =   [['&', '.', '.', '.'],
               ['.', '#', '.', '#'],
@@ -51,77 +26,23 @@ def main():
               ['#', '.', '.', '$']]
 
     env = FrozenLake(lake, slip=0.1, max_steps=16, seed=seed)
-    
-    print('# Model-based algorithms')
-    gamma = 0.9
-    theta = 0.001
-    max_iterations = 100
-    
-    print('')
-    
-    print('## Policy iteration')
-    policy, value = policy_iteration(env, gamma, theta, max_iterations)
-    env.render(policy, value)
-    
-    print('')
-    
-    print('## Value iteration')
-    policy, value = value_iteration(env, gamma, theta, max_iterations)
-    env.render(policy, value)
-    
-    print('')
-    
+
     print('# Model-free algorithms')
     max_episodes = 2000
     eta = 0.5
     epsilon = 0.5
-    
-    print('')
-    
-    print('## Sarsa')
-    policy, value = sarsa(env, max_episodes, eta, gamma, epsilon, seed=seed)
-    env.render(policy, value)
-    
-    print('')
-    
-    print('## Q-learning')
-    policy, value = q_learning(env, max_episodes, eta, gamma, epsilon, seed=seed)
-    env.render(policy, value)
-    
-    print('')
-    
+    gamma = 0.9
+
     linear_env = LinearWrapper(env)
-    
+
     print('## Linear Sarsa')
-    
+
     parameters = linear_sarsa(linear_env, max_episodes, eta, gamma, epsilon, seed=seed)
     policy, value = linear_env.decode_policy(parameters)
     linear_env.render(policy, value)
-    
-    print('')
-    
-    print('## Linear Q-learning')
-    
-    parameters = linear_q_learning(linear_env, max_episodes, eta, gamma, epsilon, seed=seed)
-    policy, value = linear_env.decode_policy(parameters)
-    linear_env.render(policy, value)
-    
-    print('')
-    
-    print('# Model-building algorithm')
-    max_episodes = 200
-    alpha = 0.01
-    rprior_variance = 2
-    r_variance = 1e-8
 
     print('')
 
-    print('## Posterior sampling policy iteration')    
-    policy, value = ps_policy_iteration(env, max_episodes, alpha, rprior_variance, r_variance, gamma, theta, max_iterations, seed=seed)
-    env.render(policy, value)
-    
-    print('')
-    
 
 if __name__ == "__main__":
     main()
--- tabular.py
+++ tabular.py
@@ -8,7 +8,7 @@ def _printoptions(*args, **kwargs):
     np.set_printoptions(*args, **kwargs)
     try:
         yield
-    finally: 
+    finally:
         np.set_printoptions(**original)
 
 
@@ -16,105 +16,91 @@ class EnvironmentModel:
     def __init__(self, n_states, n_actions, seed=None):
         self.n_states = n_states
         self.n_actions = n_actions
-        
+
         self.random_state = np.random.RandomState(seed)
-        
+
     def p(self, next_state, state, action):
         raise NotImplementedError()
-    
+
     def r(self, next_state, state, action):
         raise NotImplementedError()
-        
+
     def draw(self, state, action):
         p = [self.p(ns, state, action) for ns in range(self.n_states)]
         next_state = self.random_state.choice(self.n_states, p=p)
         reward = self.r(next_state, state, action)
-        
+
         return next_state, reward
-    
-    
-class TabularEnvironmentModel(EnvironmentModel):
-    def __init__(self, n_states, n_actions, p, r, seed=None):
-        EnvironmentModel.__init__(self, n_states, n_actions, seed)
-        
-        self._p = p
-        self._r = r
 
-    def p(self, next_state, state, action):
-        return self._p[next_state, state, action]
 
-    def r(self, next_state, state, action):
-        return self._r[next_state, state, action]
-    
-    
 class Environment(EnvironmentModel):
     def __init__(self, n_states, n_actions, max_steps, pi, seed=None):
         EnvironmentModel.__init__(self, n_states, n_actions, seed)
-        
+
         self.max_steps = max_steps
-        
+
         self.pi = pi
         if self.pi is None:
             self.pi = np.full(n_states, 1./n_states)
-        
+
     def reset(self):
         self.n_steps = 0
         self.state = self.random_state.choice(self.n_states, p=self.pi)
-        
+
         return self.state
-        
+
     def step(self, action):
         if action < 0 or action >= self.n_actions:
             raise Exception('Invalid action.')
-        
+
         self.n_steps += 1
         done = (self.n_steps >= self.max_steps)
-        
+
         self.state, reward = self.draw(self.state, action)
-        
+
         return self.state, reward, done
-    
+
     def render(self, policy=None, value=None):
         raise NotImplementedError()
-        
+
 
 class LinearWrapper:
     def __init__(self, env):
         self.env = env
-        
+
         self.n_actions = self.env.n_actions
         self.n_states = self.env.n_states
         self.n_features = self.n_actions * self.n_states
-        
+
     def encode_state(self, s):
         features = np.zeros((self.n_actions, self.n_features))
         for a in range(self.n_actions):
             i = np.ravel_multi_index((s, a), (self.n_states, self.n_actions))
             features[a, i] = 1.0
-          
+
         return features
-    
+
     def decode_policy(self, theta):
         policy = np.zeros(self.env.n_states, dtype=int)
         value = np.zeros(self.env.n_states)
-        
+
         for s in range(self.n_states):
             features = self.encode_state(s)
             q = features.dot(theta)
-            
+
             policy[s] = np.argmax(q)
             value[s] = np.max(q)
-        
+
         return policy, value
-        
+
     def reset(self):
         return self.encode_state(self.env.reset())
-    
+
     def step(self, action):
         state, reward, done = self.env.step(action)
-        
+
         return self.encode_state(state), reward, done
-    
+
     def render(self, policy=None, value=None):
         self.env.render(policy, value)
 
@@ -124,24 +110,24 @@ class FrozenLake(Environment):
         # start (&), frozen (.), hole (#), goal ($)
         self.lake = np.array(lake)
         self.lake_flat = self.lake.reshape(-1)
-        
+
         self.slip = slip
-        
+
         n_states = self.lake.size + 1
         n_actions = 4
-        
+
         pi = np.zeros(n_states, dtype=float)
         pi[np.where(self.lake_flat == '&')[0]] = 1.0
-        
+
         self.absorbing_state = n_states - 1
-        
+
         # up, left, down, right
         actions = [(-1, 0), (0, -1), (1, 0), (0, 1)]
-        
+
         self._p = np.zeros((n_states, n_states, n_actions))
-        
+
         self._p[self.absorbing_state, self.absorbing_state, :] = 1.0
-        
+
         for s in range(self.absorbing_state):
             if (self.lake_flat[s] == '#') or (self.lake_flat[s] == '$'):
                 self._p[self.absorbing_state, s, :] = 1.0
@@ -151,51 +137,50 @@ class FrozenLake(Environment):
                     c = np.unravel_index(s, self.lake.shape)
                     next_c = c[0] + actions[a][0], c[1] + actions[a][1]
                     next_s = np.ravel_multi_index(next_c, self.lake.shape, 'clip')
-                    
+
                     self._p[next_s, s, a] = 1 - self.slip
-                    
+
                     neighbors.append(next_s)
-                    
+
                 for neighbor in neighbors:
                     self._p[neighbor, s, :] += self.slip / n_actions
-                
+
         Environment.__init__(self, n_states, n_actions, max_steps, pi, seed=seed)
-        
+
     def p(self, next_state, state, action):
         return self._p[next_state, state, action]
-    
+
     def r(self, next_state, state, action):
         if state < self.absorbing_state:
             return float(self.lake_flat[state] == '$')
-        
+
         return 0.
-    
+
     def step(self, action):
         state, reward, done = Environment.step(self, action)
-        
+
         done = (state == self.absorbing_state) or done
-        
+
         return state, reward, done
-   
+
     def render(self, policy=None, value=None):
         if policy is None:
             lake = np.array(self.lake_flat)
-            
+
             if self.state < self.absorbing_state:
                 lake[self.state] = '@'
-                
+
             print(lake.reshape(self.lake.shape))
         else:
             actions = ['↑', '←', '↓', '→']
-            
+
             print('Lake:')
             print(self.lake)
-        
+
             print('Policy:')
             policy = np.array([actions[a] for a in policy[:-1]])
             print(policy.reshape(self.lake.shape))
-            
+
             print('Value:')
             with _printoptions(precision=3, suppress=True):
                 print(value[:-1].reshape(self.lake.shape))
-            
